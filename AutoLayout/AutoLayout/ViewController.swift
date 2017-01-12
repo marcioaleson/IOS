a10e6d9b51d816a5f60b26e47fe37b2a6f6569d6
marcioaleson <marcioaleson@gmail.com>
2017-01-12 12:48:38 -0300

    Auto Layout example.

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Marcio Aleson on 1/12/17.
+//  Copyright © 2017 Marcio Aleson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
