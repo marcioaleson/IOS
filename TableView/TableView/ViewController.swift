e5babd273f93292859e862ef415bb9633c7fea1c
marcioaleson <marcioaleson@gmail.com>
2017-01-15 13:47:59 -0300

    Connecting the DataSource and Delegate.

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
 
-    var restaurantNames = ["cafe Deadend", "Homei","Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Grahem Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "Cash Pub and kitchen"]
+    var restaurantNames = ["Cafe Deadend", "Homei","Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Grahem Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "Cash Pub and kitchen"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +23,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Return the number os rows in the section.
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifer = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifer, for: indexPath)
+        //Configure the cell...
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        return cell
+    }
 }
 
