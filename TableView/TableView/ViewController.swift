5fe1053bbcbb746b8541dd7e640195f3297e1fef
marcioaleson <marcioaleson@gmail.com>
2017-01-15 12:01:28 -0300

    Added append UItalbeViewDataSource and UITabelViewDelegate in ViewController.

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
 
+    var restaurantNames = ["cafe Deadend", "Homei","Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Grahem Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "Cash Pub and kitchen"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
