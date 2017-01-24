acd96578aee06f14e9e794d0c028ee04b8f6ac1a
marcioaleson <marcioaleson@gmail.com>
2017-01-23 22:44:15 -0300

    Added defferent thumbnails.

--- TableViews2/TableViews2/RestaurantTableViewController.swift
+++ TableViews2/TableViews2/RestaurantTableViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class RestaurantTableViewController: UITableViewController {
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster","For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Haigh's Chocolate", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and kitchen"]
+    
+    var restaurantImages = [ "cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "caskpubkitchen.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -44,7 +47,7 @@ class RestaurantTableViewController: UITableViewController {
 
         // Configure the cell...
         cell.textLabel?.text = restaurantNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: "restaurant.jpg")
+        cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row])
 
         return cell
     }
