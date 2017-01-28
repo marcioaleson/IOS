946ebc46ddaf35ccb1a549967f584f0276d2e365
marcioaleson <marcioaleson@gmail.com>
2017-01-27 21:47:39 -0300

    Outlet connections.

---
+++ TableViews2/TableViews2/RestaurantTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RestaurantTableViewCell.swift
+//  TableViews2
+//
+//  Created by Marcio Aleson on 27/01/17.
+//  Copyright © 2017 Marcio Aleson. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageVIew: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
